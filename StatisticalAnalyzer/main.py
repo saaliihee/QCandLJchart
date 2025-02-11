82d235e5e326c799e4c0d6248e969e93ca33601e
saaliihee <k.1v@live.com>
2025-02-11 18:56:56 +0300

    main.py

--- StatisticalAnalyzer/main.py
+++ StatisticalAnalyzer/main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import plotly.graph_objects as go
+import https://saaliihee.github.io/QCandLJchart/ as go
 import numpy as np
 from utils import calculate_statistics, validate_input
 
